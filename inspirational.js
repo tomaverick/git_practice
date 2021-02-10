ffa4a4127d5cd6ed2f12ec8a28db6fc3baafc1e1
TomJMtz <23747199+tomaverick@users.noreply.github.com>
2021-02-10 13:36:42 +0100

    organized related code in object

--- inspirational.js
+++ inspirational.js
@@ -1,13 +1,17 @@
-let part1 = ["Life is ", "The result in life is ", 
+let inspireQuote = {
+    _part1: [
+        "Life is ", "The result in life is ", 
 "All you need to remember is ", "The most important thing is ", 
-"The wise one knows "];
-
-let part2 = ["that love ", "that work ", "that money ", 
+"The wise one knows "
+    ],
+    _part2: [
+        "that love ", "that work ", "that money ", 
 "that power ", "that wealth ", "that luxury living ", 
 "that money making mindest ", "that ownership ", "that leadership ", 
-"that your leadership ", "your money ", "your wealth "];
-
-let part3 = ["is infinite.", "is abundant.", "everywhere and always.", 
+"that your leadership ", "your money ", "your wealth "
+    ],
+    _part3: [
+        "is infinite.", "is abundant.", "everywhere and always.", 
 "existing in its purest form.", "is for the good of mankind", 
 "is valuable.", "comes from love.", 
 "is so practical to life that life wouldn't be fair without it.", 
@@ -17,14 +21,15 @@ let part3 = ["is infinite.", "is abundant.", "everywhere and always.",
 "is a life quality multiplier.", "is whatever you choose it to be for you.",
 "is whatever you choose to believe it is.", 
 "is not to be feared, but to be taken up responsibly.", 
-"is to be appreciated"];
+"is to be appreciated"
+    ],
+    generateInspiration() {
+        let textIndex1 = Math.floor(Math.random()*this._part1.length);
+        let textIndex2 = Math.floor(Math.random()*this._part2.length);
+        let textIndex3 = Math.floor(Math.random()*this._part3.length);
+    
+        return this._part1[textIndex1] + this._part2[textIndex2] + this._part3[textIndex3];
+    }
+} 
 
-function generateInspiration(){
-    let textIndex1 = Math.floor(Math.random()*part1.length);
-    let textIndex2 = Math.floor(Math.random()*part2.length);
-    let textIndex3 = Math.floor(Math.random()*part3.length);
-
-    return part1[textIndex1] + part2[textIndex2] + part3[textIndex3];
-}
-
-console.log(generateInspiration());
+console.log(inspireQuote.generateInspiration());
